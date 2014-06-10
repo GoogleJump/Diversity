3a3bdd3ab03f39f410c0a99f73fa6bd91486a64e
Stephanie New <syqnew@mit.edu>
2014-06-09 21:47:08 -0700

    Map View looks like mockup. Adding functionality next

--- ParseStarterProject/src/com/parse/starter/MapActivity.java
+++ ParseStarterProject/src/com/parse/starter/MapActivity.java
@@ -2,13 +2,23 @@ package com.parse.starter;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MapActivity extends Activity {
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
+		// Remove title bar
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+		// Remove notification bar
+		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+		// set content view AFTER ABOVE sequence (to avoid crash)
 		setContentView(R.layout.map);
 	}
 
